99c7b1c362085ef5e06246739e0554f387d3b2a4
Jean Luc Habimana <jeanluchabi@gmail.com>
2023-06-20 14:32:46 -0700

    0x02. C - Functions, nested loops

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * Descriotin 'print alphabets except q and e'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+	while (n <= 122)
+	{
+		if (n == 101 || 113)
+		        n++;
+			continue;
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
