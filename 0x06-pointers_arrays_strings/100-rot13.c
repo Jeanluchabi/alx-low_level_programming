4a4c902365704ccb1b770c4812ad1e2eccf6806d
Jean Luc Habimana <jeanluchabi@gmail.com>
2023-06-30 07:31:33 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,10 +3,11 @@
 
 /**
  * rot13 - encoder rot13
- * @s: pointer to string parameters
+ * @s: pointer to string params
+ *
  * Return: *s
  */
-char *rot13(char *)
+char *rot13(char *s)
 {
 	int i;
 	int j;
