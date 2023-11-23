c36362c66f6f8f00c3663b035e329f71076d8d2f
Jean Luc HABIMANA <jeanluchabi@gmail.com>
2023-11-23 03:02:15 -0800

    102-result

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -5,7 +5,8 @@ largest_palindrome = 0
 for x in range(100, 1000):
     for y in range(100, 1000):
         result = x * y
-        if str(result) == str(result) [::-1] and result > largest_palindrome:
+        str_result = str(result)
+        if str_result == str_result [::-1] and result > largest_palindrome:
             largest_palindrome = result
 
 with open("102-result", "w") as file:
